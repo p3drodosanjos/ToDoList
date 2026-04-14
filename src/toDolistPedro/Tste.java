b32a32ddec313c4051602abd6e7d2d86784f5970
p3drodosanjos <pedrojasiel18@gmail.com>
2026-04-14 19:07:42 -0300

    Teste

---
+++ src/toDolistPedro/Tste.java
@@ -0,0 +1,18 @@
+package toDolistPedro;
+
+public class Tste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String Mensagem  = "Pedro";
+		
+		
+		System.out.println(Mensagem);
+	
+		
+
+	}
+
+}
